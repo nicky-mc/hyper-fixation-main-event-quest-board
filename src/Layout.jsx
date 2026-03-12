ba79cefd3711643cf7f4ac9b2d72aafa97be794b
Nicky Mortoza-Cowles <nicky.mortoza-cowles@techeducators.co.uk>
2026-03-12 22:28:48 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -312,5 +312,6 @@ export default function Layout({ children, currentPageName }) {
 
       <MessageToast currentPageName={currentPageName} />
     </div>
+    </AdventurerContext.Provider>
   );
 }
