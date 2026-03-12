d28cab9a4dd8dde35dc5c090e8890ddf01b15933
Nicky Mortoza-Cowles <nicky.mortoza-cowles@techeducators.co.uk>
2026-03-12 23:49:28 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import NavigationTracker from '@/lib/NavigationTracker'
+
 import { pagesConfig } from './pages.config'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
